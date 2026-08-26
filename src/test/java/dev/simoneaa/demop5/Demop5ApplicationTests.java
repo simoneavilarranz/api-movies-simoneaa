2ae87b3d032440966ef5945ee30b85e9d8b0ab6d
unknown <avilarranz@gmail.com>
2026-08-26 12:28:44 +0200

    first commit

---
+++ src/test/java/dev/simoneaa/demop5/Demop5ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.simoneaa.demop5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demop5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

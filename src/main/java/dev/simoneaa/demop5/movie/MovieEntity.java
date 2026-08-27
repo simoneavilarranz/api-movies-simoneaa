6b39c3323915a09875bc4bb7dd1278ac0cb3c12f
unknown <avilarranz@gmail.com>
2026-08-27 12:07:20 +0200

    feat:added director and length and getters + setters

--- src/main/java/dev/simoneaa/demop5/movie/MovieEntity.java
+++ src/main/java/dev/simoneaa/demop5/movie/MovieEntity.java
@@ -12,14 +12,52 @@ public class MovieEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+    private String title;
+    private String director;
+    private int length;
 
     public MovieEntity() {
     }
 
-    public MovieEntity(Long id, String name) {
+    public MovieEntity(Long id, String title, String director, int length) {
         this.id = id;
-        this.name = name;
+        this.title = title;
+        this.director = director;
+        this.length = length;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    
 
 }

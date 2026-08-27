eed62368c1c6b414bddefd88d37c6a86ed1d5cd6
unknown <avilarranz@gmail.com>
2026-08-27 14:45:08 +0200

    feat:created movie dto response

---
+++ src/main/java/dev/simoneaa/demop5/dtos/MovieDTOResponse.java
@@ -0,0 +1,5 @@
+package dev.simoneaa.demop5.dtos;
+
+public record MovieDTOResponse(Long id, String title, String director, int length) {
+    
+}
--- src/main/java/dev/simoneaa/demop5/movie/MovieEntity.java
+++ src/main/java/dev/simoneaa/demop5/movie/MovieEntity.java
@@ -58,6 +58,4 @@ public class MovieEntity {
         this.length = length;
     }
 
-    
-
 }

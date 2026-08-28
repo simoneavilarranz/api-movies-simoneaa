eece25217f623158c0e647d0cddda2e9d23fe84d
unknown <avilarranz@gmail.com>
2026-08-28 12:36:35 +0200

    feat:created moviecontroller implementing movie dto response

---
+++ src/main/java/dev/simoneaa/demop5/movie/MovieController.java
@@ -0,0 +1,28 @@
+package dev.simoneaa.demop5.movie;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+import dev.simoneaa.demop5.implementations.InterfaceGenericGetService;
+import dev.simoneaa.demop5.movie.dtos.MovieDTOResponse;
+
+@RestController
+@RequestMapping(path = "${api-endpoint}/movies")
+public class MovieController {
+
+    private final InterfaceGenericGetService<MovieDTOResponse> getService;
+
+    public MovieController(
+        InterfaceGenericGetService<MovieDTOResponse> getService
+    ) { this.getService = getService;
+    }
+    
+    @GetMapping("")
+    public List<MovieDTOResponse> index() {
+        return getService.getEntities();
+    }
+    
+}

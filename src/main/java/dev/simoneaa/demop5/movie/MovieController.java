2701b4fb354dcca8900b858cb448568505439711
unknown <avilarranz@gmail.com>
2026-08-28 14:10:56 +0200

    feat:succesful test of movie service impl get entities method

--- src/main/java/dev/simoneaa/demop5/movie/MovieController.java
+++ src/main/java/dev/simoneaa/demop5/movie/MovieController.java
@@ -1,7 +1,6 @@
 package dev.simoneaa.demop5.movie;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
---
+++ src/test/java/dev/simoneaa/demop5/movie/MovieServiceImplTest.java
@@ -0,0 +1,47 @@
+package dev.simoneaa.demop5.movie;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import dev.simoneaa.demop5.movie.dtos.MovieDTOResponse;
+
+@ExtendWith(MockitoExtension.class)
+public class MovieServiceImplTest {
+    @InjectMocks
+    private MovieServiceImpl service;
+
+    @Mock
+    private MovieRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        service = new MovieServiceImpl(repository);
+    }
+
+    @Test
+    void testGetEntities() {
+        List<MovieEntity> moviesMock = List.of(
+                new MovieEntity(1L, "Inception", "Christopher Nolan", 148),
+                new MovieEntity(2L, "Cure", "Kiyoshi Kurosawa", 111)
+        );
+
+        when(repository.findAll()).thenReturn(moviesMock);
+        List<MovieDTOResponse> movies = service.getEntities();
+
+        assertThat(movies.size()).isEqualTo(2);
+        assertThat(movies.get(0).title()).isEqualTo("Inception");
+        assertThat(movies.get(0).director()).isEqualTo( "Christopher Nolan");
+        assertThat(movies.get(0).length()).isEqualTo(148);
+        assertThat(movies.get(1).title()).isEqualTo("Cure");
+        assertThat(movies.get(1).director()).isEqualTo( "Kiyoshi Kurosawa");
+        assertThat(movies.get(1).length()).isEqualTo(111);
+    }
+}

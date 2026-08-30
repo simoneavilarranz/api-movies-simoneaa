8b07e93ef6e7705179c7c4de1d24e81435b06293
unknown <avilarranz@gmail.com>
2026-08-30 14:06:31 +0200

    feat:implemented edit service in controller and added post mapping method

--- src/main/java/dev/simoneaa/demop5/movie/MovieController.java
+++ src/main/java/dev/simoneaa/demop5/movie/MovieController.java
@@ -1,23 +1,33 @@
 package dev.simoneaa.demop5.movie;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
+import dev.simoneaa.demop5.implementations.InterfaceGenericEditService;
 import dev.simoneaa.demop5.implementations.InterfaceGenericGetService;
+import dev.simoneaa.demop5.movie.dtos.MovieDTORequest;
 import dev.simoneaa.demop5.movie.dtos.MovieDTOResponse;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(path = "${api-endpoint}/movies")
 public class MovieController {
 
     private final InterfaceGenericGetService<MovieDTOResponse> getService;
+    private final InterfaceGenericEditService<MovieDTORequest, MovieDTOResponse> editService;
 
     public MovieController(
-        InterfaceGenericGetService<MovieDTOResponse> getService
+        InterfaceGenericGetService<MovieDTOResponse> getService,
+        InterfaceGenericEditService<MovieDTORequest, MovieDTOResponse> editService
     ) { this.getService = getService;
+        this.editService = editService;
     }
     
     @GetMapping("")
@@ -28,6 +38,18 @@ public class MovieController {
     @GetMapping("{id}")
     public MovieDTOResponse getById(@PathVariable Long id) {
         return getService.getById(id);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<MovieDTOResponse> store(@Valid @RequestBody MovieDTORequest dto) {
+
+        MovieDTOResponse dtoResponse = editService.storeEntity(dto);
+
+        if (dtoResponse == null)
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+
+        return ResponseEntity.status(201).body(dtoResponse);
+
     }
     
 }

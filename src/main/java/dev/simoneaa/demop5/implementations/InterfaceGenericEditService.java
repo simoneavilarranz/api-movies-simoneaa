65b95cbccb219e63abe98dc993278b30582c2cf9
unknown <avilarranz@gmail.com>
2026-08-31 00:51:37 +0200

    feat:added delete method to generic edit service

--- src/main/java/dev/simoneaa/demop5/implementations/InterfaceGenericEditService.java
+++ src/main/java/dev/simoneaa/demop5/implementations/InterfaceGenericEditService.java
@@ -3,4 +3,5 @@ package dev.simoneaa.demop5.implementations;
 public interface InterfaceGenericEditService<T, S> {
     public S storeEntity(T dto);
     public S updateEntity(Long id, T dto);
+    public void deleteEntity(Long id);
 }

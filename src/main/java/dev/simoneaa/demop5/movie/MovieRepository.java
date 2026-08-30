0d49f2d70f442f982690899e6ffd3e4bc6d62653
unknown <avilarranz@gmail.com>
2026-08-31 01:00:28 +0200

    feat:added find by title method to repository

--- src/main/java/dev/simoneaa/demop5/movie/MovieRepository.java
+++ src/main/java/dev/simoneaa/demop5/movie/MovieRepository.java
@@ -1,7 +1,8 @@
 package dev.simoneaa.demop5.movie;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface MovieRepository extends JpaRepository<MovieEntity, Long> {
-    
+    Optional<MovieEntity> findByTitle(String title);
 }

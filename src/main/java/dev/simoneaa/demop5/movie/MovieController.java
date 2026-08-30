7dc540708e3baeebe78dbcd2fd7e107c0d6e78aa
unknown <avilarranz@gmail.com>
2026-08-30 14:50:39 +0200

    feat:added update method to movie controller

--- src/main/java/dev/simoneaa/demop5/movie/MovieController.java
+++ src/main/java/dev/simoneaa/demop5/movie/MovieController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,5 +52,9 @@ public class MovieController {
         return ResponseEntity.status(201).body(dtoResponse);
 
     }
-    
+
+    @PutMapping("{id}")
+    public MovieDTOResponse update(@PathVariable Long id, @Valid @RequestBody MovieDTORequest dto) {
+        return editService.updateEntity(id, dto);
+    }
 }

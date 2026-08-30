852241aa6e8e511d8c456ac10da485ad1afd69a3
unknown <avilarranz@gmail.com>
2026-08-30 13:50:07 +0200

    feat:completed movie mapper by adding toentity method

--- src/main/java/dev/simoneaa/demop5/movie/mappers/MovieMapper.java
+++ src/main/java/dev/simoneaa/demop5/movie/mappers/MovieMapper.java
@@ -1,9 +1,18 @@
 package dev.simoneaa.demop5.movie.mappers;
 
 import dev.simoneaa.demop5.movie.MovieEntity;
+import dev.simoneaa.demop5.movie.dtos.MovieDTORequest;
 import dev.simoneaa.demop5.movie.dtos.MovieDTOResponse;
 
 public class MovieMapper {
+
+    public static MovieEntity toEntity(MovieDTORequest dtoRequest) {
+        MovieEntity movie = new MovieEntity();
+        movie.setTitle(dtoRequest.title());
+        movie.setDirector(dtoRequest.director());
+        movie.setLength(dtoRequest.length());
+        return movie;
+    }
 
     public static MovieDTOResponse toDTO(MovieEntity entity) {
         MovieDTOResponse dtoResponse = new  MovieDTOResponse(entity.getId(), entity.getTitle(), entity.getDirector(), entity.getLength());

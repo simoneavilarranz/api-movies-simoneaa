f4852d66fe439af0d47e05104bb9c674f8c54f7d
unknown <avilarranz@gmail.com>
2026-08-30 14:29:34 +0200

    feat:succesful controller post method test

--- src/test/java/dev/simoneaa/demop5/movie/MovieControllerTest.java
+++ src/test/java/dev/simoneaa/demop5/movie/MovieControllerTest.java
@@ -3,19 +3,24 @@ package dev.simoneaa.demop5.movie;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.MediaType;
 
+import dev.simoneaa.demop5.implementations.InterfaceGenericEditService;
 import dev.simoneaa.demop5.implementations.InterfaceGenericGetService;
+import dev.simoneaa.demop5.movie.dtos.MovieDTORequest;
 import dev.simoneaa.demop5.movie.dtos.MovieDTOResponse;
 import dev.simoneaa.demop5.movie.exceptions.MovieExceptionNotFound;
 import tools.jackson.databind.ObjectMapper;
@@ -27,6 +32,9 @@ public class MovieControllerTest {
 
     @MockitoBean
     private InterfaceGenericGetService<MovieDTOResponse> service;
+
+    @MockitoBean
+    private InterfaceGenericEditService<MovieDTORequest, MovieDTOResponse> editService;
 
     @Autowired
     ObjectMapper mapper;
@@ -72,5 +80,25 @@ public class MovieControllerTest {
 
         mockMvc.perform(get("/api/v1/movies/1"))
             .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void testPost_ShouldSave() throws Exception {
+        MovieDTORequest request = new MovieDTORequest("Cure", "Kiyoshi Kurosawa", 111);
+        MovieDTOResponse dtoResponse = new MovieDTOResponse(1L, "Cure", "Kiyoshi Kurosawa", 111);
+        String json = mapper.writeValueAsString(request);
+        String jsonResponse = mapper.writeValueAsString(dtoResponse);
+
+        when(editService.storeEntity(Mockito.any(MovieDTORequest.class))).thenReturn(dtoResponse);
+
+        MockHttpServletResponse response = mockMvc.perform(post("/api/v1/movies")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(json))
+        .andExpect(status().isCreated())
+        .andReturn()
+        .getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(201);
+        assertThat(response.getContentAsString()).isEqualTo(jsonResponse);
     }
 }

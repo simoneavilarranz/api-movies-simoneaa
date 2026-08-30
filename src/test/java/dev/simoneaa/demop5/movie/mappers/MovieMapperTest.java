60d8981e3f3c703a8adc6cef45260e2f8cbca9c8
unknown <avilarranz@gmail.com>
2026-08-30 14:10:08 +0200

    feat:succesful movie mapper to entity test

--- src/test/java/dev/simoneaa/demop5/movie/mappers/MovieMapperTest.java
+++ src/test/java/dev/simoneaa/demop5/movie/mappers/MovieMapperTest.java
@@ -1,10 +1,10 @@
 package dev.simoneaa.demop5.movie.mappers;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.Test;
 
 import dev.simoneaa.demop5.movie.MovieEntity;
+import dev.simoneaa.demop5.movie.dtos.MovieDTORequest;
 import dev.simoneaa.demop5.movie.dtos.MovieDTOResponse;
 
 public class MovieMapperTest {
@@ -17,6 +17,15 @@ public class MovieMapperTest {
         assertThat(response.title()).isEqualTo("Inception");
         assertThat(response.director()).isEqualTo("Christopher Nolan");
         assertThat(response.length()).isEqualTo(148);
+    }
+
+    @Test
+    public void toEntityTest() {
+        MovieDTORequest request = new MovieDTORequest("Inception", "Christopher Nolan", 148);
+        MovieEntity entity = MovieMapper.toEntity(request);
+        assertThat(entity.getTitle()).isEqualTo("Inception");
+        assertThat(entity.getDirector()).isEqualTo("Christopher Nolan");
+        assertThat(entity.getLength()).isEqualTo(148);
     }
 
 }

c7887663f993e9bc08a38ecb89382014d5629216
unknown <avilarranz@gmail.com>
2026-08-31 01:22:17 +0200

    feat:added find by genres name to movie repository

--- src/main/java/dev/simoneaa/demop5/movie/MovieRepository.java
+++ src/main/java/dev/simoneaa/demop5/movie/MovieRepository.java
@@ -2,7 +2,9 @@ package dev.simoneaa.demop5.movie;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
+import java.util.List;
 
 public interface MovieRepository extends JpaRepository<MovieEntity, Long> {
     Optional<MovieEntity> findByTitle(String title);
+    List<MovieEntity> findByGenresName(String genreName);
 }
